5e09f33ec85f1c8ea7801bdc09f07003831449f9
Marcelo Vasques <msouvas@gmail.com>
2013-08-12 10:21:55 -0300

    Adicao das novas funcionalidades da lista. Ainda pode haver bugs

--- src/comp_list.c
+++ src/comp_list.c
@@ -3,14 +3,21 @@
 
 typedef struct comp_list_t {
 	struct comp_list_t *proximo;
+	int chave;
 	struct comp_list_t *anterior;
 } NODO;
 
 NODO* criaLista()
 {
+	return NULL;
+}
+
+NODO* criaNodo(int chave)
+{
 	NODO *novoNodo;
 
 	novoNodo = malloc(sizeof(NODO));
+	novoNodo->chave = chave;
 	novoNodo->proximo = NULL;
 	novoNodo->anterior = NULL;
 
@@ -23,20 +30,73 @@ NODO* insereNodo(NODO* novoNodo, NODO* inicioLista)
 	
 	aux = inicioLista;
 
-	while(aux->proximo!=NULL) {
-		aux = aux->proximo;
+	if(aux==NULL) {
+		aux = novoNodo;
+		inicioLista = aux;
 	}
-	
-	aux->proximo = novoNodo;
-	novoNodo->anterior = aux;
-	novoNodo->proximo = NULL;
-	aux = novoNodo;
+	else {
+		while(aux->proximo!=NULL && aux!=NULL) {
+			aux = aux->proximo;
+		}
+		aux->proximo = novoNodo;
+		novoNodo->anterior = aux;
+		novoNodo->proximo = NULL;
+		aux = novoNodo;
+	}
 
-	return aux;
+	return inicioLista;
 }
 
+NODO* excluiNodo(NODO* nodoExcluido, NODO* inicioLista)
+{
+	NODO* auxProx;
+	NODO* auxAnt;
 
+	auxProx = inicioLista;
 
+	while(auxProx->proximo!=nodoExcluido && auxProx->proximo!=NULL) {
+		auxProx = auxProx->proximo;
+	}
 
+	if(auxProx->proximo == nodoExcluido) {
+		auxAnt = auxProx;
+		auxProx->proximo = nodoExcluido->proximo;
+		auxProx = nodoExcluido->proximo;
+
+		if(auxProx!=NULL) {
+			auxProx->anterior = auxAnt;
+		}
+	}
+	return inicioLista;
+}
+
+void imprimeLista(NODO* inicioLista)
+{
+	NODO* aux;
+
+	aux = inicioLista;
+
+	while(aux!=NULL) {
+		printf("%i\n", aux->chave);
+		aux = aux->proximo;
+	}
+}
+
+int main()
+{
+	NODO* inicioLista;
+	NODO* nodo1;
+	NODO* nodo2;
+
+	inicioLista = criaLista();
+	
+	nodo1 = criaNodo(1);
+	nodo2 = criaNodo(2);
+
+	inicioLista = insereNodo(nodo1, inicioLista);
+	inicioLista = insereNodo(nodo2, inicioLista);
+
+	imprimeLista(inicioLista);
+}
 
 
