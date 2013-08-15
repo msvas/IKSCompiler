c530c5fe219948e7e43d14bd14a523f079cc50f2
Éder F. Zulian <ederfzul@gmail.com>
2013-08-15 05:36:55 -0300

    Some progress in regex.
    Also, main.c was modified in order to obtain the input file as a
    parameter.

--- src/main.c
+++ src/main.c
@@ -1,5 +1,8 @@
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
 #include "comp_dict.h"
 #include "comp_list.h"
 #include "comp_tree.h"
@@ -7,18 +10,31 @@
 #include "tokens.h"
 #include "util.h"
 
-int getLineNumber (void)
+int getLineNumber(void)
 {
-	/* deve ser implementada */
-	return 0;
+	return get_line();
 }
 
-int main (int argc, char **argv)
+int main(int argc, char *argv[])
 {
 	int token = TOKEN_ERRO;
-	while (token = yylex()){
-		printf("token <%d> at %d\n", token, getLineNumber());
+
+	if (argc < 2 || argc > 2) {
+		puts("\n");
+		puts("Wrong number of arguments");
+		puts("Try:");
+		printf("\t%s <input file>\n", argv[0]);
+		puts("\n");
+		exit(EXIT_FAILURE);
 	}
+
+	/* Set flex to read from a file instead of stdin */
+	set_yyin(argv[1]);
+
+	while (token = yylex()) {
+		//printf("token <%d> at line %d\n", token, getLineNumber());
+	}
+
 	return 0;
 }
 
