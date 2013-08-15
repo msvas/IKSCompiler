bb5a082824b27d9ed9607251369828da6429e206
Hugo Constantinopolos <hugo.cpolos@gmail.com>
2013-08-14 22:35:51 -0300

    tested n-ary tree

--- src/comp_tree.c
+++ src/comp_tree.c
@@ -1,55 +1,89 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define NUMERO_FILHOS 2
 
-typedef struct comp_tree_t {
-	comp_tree_ t *right;
-	comp_tree_t *left;
-	int data;
+typedef struct comp_tree {
+	struct comp_tree *filhos[NUMERO_FILHOS];
+	int chave;
 }NODO;
 
-NODO* cria_arvore();
-{
-	return NULL;
-}
 
-NODO* cria_nodo(int chave)
+
+NODO* criaNodo(int chave)
 {
-	NODO *novoNodo;
-	
+	NODO* novoNodo;
+	int indice;
 	novoNodo = malloc(sizeof(NODO));
 	novoNodo->chave = chave;
-	novoNodo->right = NULL;
-	novoNodo->left = NULL;
-
+	for(indice = 0; indice < NUMERO_FILHOS; indice++)
+		novoNodo->filhos[indice] = NULL;
 	return novoNodo;
 }
 
-NODO* insereNodo(NODO* novoNodo, NODO* raiz)
+
+void insereNodo(NODO* novoNodo, NODO* raiz)
 {
+	int indice;
+	for(indice=0;indice<NUMERO_FILHOS;indice++)
+		if(raiz->filhos[indice] == NULL)
+		{
+			raiz->filhos[indice] = novoNodo;
+			return; 	
+		}
+	indice = 0;
+	while(indice < NUMERO_FILHOS)
+	{
+		if(raiz->filhos[indice]->filhos[NUMERO_FILHOS-1] != NULL)
+			indice++;
+		else
+		{
+			insereNodo(novoNodo, raiz->filhos[indice]);
+			return;
+		}
+	}
+	insereNodo(novoNodo, raiz->filhos[0]);
 	
-	if(novoNodo->chave >= raiz->chave)
-	{
-		if(raiz->right == NULL)
+	
+}
+
+void imprimeArvore(NODO* raiz)
+{
+	int indice;
+	printf("%d filhos: ", raiz->chave);
+	for(indice=0;indice<NUMERO_FILHOS;indice++)
+		if(raiz->filhos[indice] != NULL)		
 		{
-			raiz->right = novoNodo;
-		}
-		else
+			printf("%d ", raiz->filhos[indice]->chave);			
+			
+		} 	
+	for(indice=0;indice<NUMERO_FILHOS;indice++)
+		if(raiz->filhos[indice] != NULL)		
 		{
-			insereNodo(novoNodo, raiz->right);
+			printf("\n");			
+			imprimeArvore(raiz->filhos[indice]);
 		}
-	}
-	else
-	{
-		if(raiz->left == NULL)
-		{
-			raiz->left = novoNodo;
-		}
-		else
-		{
-			insereNodo(novoNodo, raiz->left)
-		}
-	}
 }
 
 
+void main()
+{
+	NODO* raiz;
+	int chaves;
+	int resposta = 1;
+	
+	printf("insira o primeiro nó: ");
+	scanf("%d", &chaves);
+	raiz = criaNodo(chaves);
+	
+	while(resposta == 1)
+	{
+		printf("insira um nó: ");
+		scanf("%d", &chaves);
+		insereNodo(criaNodo(chaves),raiz);
+		printf("continuar?(1/0): ");
+		scanf("%d", &resposta);
+	}
+
+	imprimeArvore(raiz);
+}
 
