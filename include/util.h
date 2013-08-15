181f84ea3be617ce59baf3185e5b59bff7d87d93
Éder F. Zulian <ederfzul@gmail.com>
2013-08-15 00:27:54 -0300

    Merge problems fixed.
    printdbg() --> debug()

--- include/util.h
+++ include/util.h
@@ -15,11 +15,11 @@
  * Print utility that prints file and line. Useful for debug. It can replace
  * printf() with advantages. Usage is almost the same as printf().
  * E.g.:
- * printdbg("%s", "Hello world");
+ * debug("%s", "Hello world");
  */
-#define printdbg(fmt, ...) printf("%s (%d): " fmt "\n", __FILE__, __LINE__, ##__VA_ARGS__)
+#define debug(fmt, ...) printf("%s (%d): " fmt "\n", __FILE__, __LINE__, ##__VA_ARGS__)
 #else
-#define printdbg(fmt, ...)
+#define debug(fmt, ...)
 #endif /* DEBUG_ON */
 
 #endif /* __UTIL_H__ */
--- src/main.c
+++ src/main.c
@@ -4,29 +4,21 @@
 #include "comp_list.h"
 #include "comp_tree.h"
 #include "comp_graph.h"
-<<<<<<< HEAD
-#include "util.h"
-
-int main (int argc, char **argv)
-{
-	printdbg("Main function...");
-	return 0;
-=======
 #include "tokens.h"
+#include "util.h"
 
 int getLineNumber (void)
 {
-  /* deve ser implementada */
-  return 0;
+	/* deve ser implementada */
+	return 0;
 }
 
 int main (int argc, char **argv)
 {
-  int token = TOKEN_ERRO;
-  while (token = yylex()){
-    printf ("token <%d> at %d\n", token, getLineNumber());
-  }
-  return 0;
->>>>>>> schnorr/master
+	int token = TOKEN_ERRO;
+	while (token = yylex()){
+		printf("token <%d> at %d\n", token, getLineNumber());
+	}
+	return 0;
 }
 
