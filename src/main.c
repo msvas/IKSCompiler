e9f946a5cba24cda998df092831eb0052de5efc6
Éder F. Zulian <ederfzul@gmail.com>
2013-10-09 03:12:39 -0300

    Usage: 	./main <input_file>

--- src/main.c
+++ src/main.c
@@ -10,6 +10,14 @@ int main(int argc, char *argv[])
 {
 	int r;
 
+	if (argc != 2) {
+		printf("\nUsage:\n");
+		printf("\t%s <input_file>\n\n", argv[0]);
+		exit(-1);
+	}
+
+	set_yyin(argv[1]);
+
 	gv_init("visual_tree.dot");
 	r = yyparse();
 	gv_close();
