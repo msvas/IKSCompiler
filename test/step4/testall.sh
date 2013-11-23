880ddf60886ff5ea52588422d9d12a448ae58f5d
Éder F. Zulian <ederfzul@gmail.com>
2013-11-23 00:55:57 -0200

    Script for tests fixed.
    - Input path fixed.
    - Variable name in lower case.
    - Tiny improvment in error message readability.

--- test/step4/testall.sh
+++ test/step4/testall.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-INPUTS_PATH="./"
+INPUTS_PATH="."
 BIN_PATH="../../build"
 BIN=$BIN_PATH/main
 REPORT=report
@@ -22,17 +22,17 @@ check_if_command_exists () {
 	type $1 >/dev/null 2>&1 || { echo -e >&2 "\033[41mI require $1 but it's not installed. Please install it and try again. Aborting.\033[40m"; exit 1; }
 }
 
-for INPUT in `ls -1 $INPUTS_PATH/s*.iks`
+for input in `ls -1 $INPUTS_PATH/s*.iks`
 do
-	EXPECTED_ERROR=`head -n1 $INPUT | awk '{ print $2 }'`
+	EXPECTED_ERROR=`head -n1 $input | awk '{ print $2 }'`
 	EXPECTED_ERROR_CODE=`grep "$EXPECTED_ERROR " $INPUTS_PATH/errors.h | awk '{ print $3 }'`
-	$BIN < $INPUT > /dev/null 2> /dev/null #$REPORT
+	$BIN < $input > /dev/null 2> /dev/null #$REPORT
 	GOT_ERROR_CODE=$?
 	
 	#detect segfault
 	if [ $GOT_ERROR_CODE -eq 139 ]
 	then
-		R="$INPUT causes a segfault (exit code is $GOT_ERROR_CODE)"
+		R="$input causes a segfault (exit code is $GOT_ERROR_CODE)"
 		echo $R
 		echo $R >> $REPORT
 		continue
@@ -42,7 +42,7 @@ do
 	check_if_command_exists valgrind
 	TEMP_FILE="/tmp/e4-$RANDOM.tmp"
 	rm -f $TEMP_FILE
-	valgrind $BIN < $INPUT 2>> $TEMP_FILE >> $TEMP_FILE
+	valgrind $BIN < $input 2>> $TEMP_FILE >> $TEMP_FILE
 	INVALID_WRITES=`cat $TEMP_FILE | grep "Invalid write" | wc -l`
 	INVALID_READS=`cat $TEMP_FILE | grep "Invalid read" | wc -l`
 	CONDITIONAL=`cat $TEMP_FILE | grep "Conditional" | wc -l`
@@ -70,12 +70,12 @@ do
 	if [ $GOT_ERROR_CODE -ne $EXPECTED_ERROR_CODE ]
 	then
 		N=`grep " $GOT_ERROR_CODE " $INPUTS_PATH/errors.h | awk '{ print $2 }'`
-		R="$INPUT expects a $EXPECTED_ERROR($EXPECTED_ERROR_CODE), but we got a $N($GOT_ERROR_CODE) $CODE_CORRECT"
+		R="$input expects a $EXPECTED_ERROR ($EXPECTED_ERROR_CODE), but we got a $N ($GOT_ERROR_CODE) $CODE_CORRECT"
 		echo $R
 		echo $R >> $REPORT
 	else
 		((nota = $nota + 1))
-		R="$INPUT $EXPECTED_ERROR okay $CODE_CORRECT"
+		R="$input $EXPECTED_ERROR okay $CODE_CORRECT"
 		echo $R
 		echo $R >> $REPORT
 	fi
